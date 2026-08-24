f09c5ca81c760d51e324220d2db9595e1890cc9e
Mohd-Furkhan-lab <mohdfurkhan2255@gmail.com>
2026-08-24 11:25:13 +0530

    Clean up token handling in new_access_token
    Removed debug print statements for tokens.

--- routes/user.py
+++ routes/user.py
@@ -48,9 +48,7 @@ async def user_logout(request:Request,response:Response):
 @users.post('/refresh')
 def new_access_token(request:Request,response:Response):
     token = request.cookies.get("refresh")
-    print(token)
     new_token = refresh(token)
-    print(new_token)
     response.set_cookie(
         key = "access",
         value = new_token,
